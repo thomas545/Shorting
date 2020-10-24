1c39e55020911ab72a840d879b77c7671ac02135
thomas434 <thomas@dokkanz.com>
2020-10-24 17:11:28 +0200

    add to deployments

--- shorting/settings/base.py
+++ shorting/settings/base.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / "statics"]
 
-HOST_NAME = "http://localhost:9000/"
+HOST_NAME = "http://cuturls.live/"
--- shorting/settings/production.py
+++ shorting/settings/production.py
@@ -2,4 +2,4 @@ from .development import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["HOST"]
+ALLOWED_HOSTS = ["cuturls.live", "www.cuturls.live"]
